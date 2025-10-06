effe26895a48833386ef0763b58488bec8195576
Federico <luka.sktt1@gmail.com>
2025-10-06 18:04:45 -0300

    Agrego menú y panel admin funcional

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,20 @@
-import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-export default function App(){ return (<div><nav className='p-4 bg-green-600 text-white'><Link to='/'>Inicio</Link></nav><Routes><Route path='/' element={<Home/>}/></Routes></div>) }
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Admin from './pages/Admin';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
